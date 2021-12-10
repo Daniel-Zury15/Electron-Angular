f868dfaf0220189f0f3476b0989f10b6d9d703de
Daniel_Zury15 <90483966+Daniel-Zury15@users.noreply.github.com>
2021-12-10 00:08:23 -0300

    10/12/2021

--- main.js
+++ main.js
@@ -1,26 +1,3 @@
-# Creating a Project
-
-Install Angular:
-
-```
-$ npm install -g @angular/cli
-```
-
-```
-$ ng new electron-app
-$ cd electron-app
-$ ng serve
-```
-
-Install Electron:
-
-```
-$ npm install --save-dev electron@latest
-```
-
-Create one `main.js`:
-
-```
 const { app, BrowserWindow } = require('electron')
 const path = require('path')
 
@@ -53,36 +30,3 @@ app.whenReady().then(() => {
 app.on('window-all-closed', () => {
   if (process.platform !== 'darwin') app.quit()
 })
-```
-
-To edit `package.json`:
-
-```
-{
-  "name": "electron_angular",
-  "version": "0.0.0",
-  "main": "main.js",
-  "scripts": {
-    "ng": "ng",
-    "start": "ng build --base-href ./ && electron .",
-    "build": "ng build",
-    "test": "ng test",
-    "lint": "ng lint",
-    "e2e": "ng e2e"
-  },
-// [...]
-```
-
-Updating dependencies
-
-```
-$ npm install
-```
-
-Run the application:
-
-```
-$ npm start
-```
-
-Finished
